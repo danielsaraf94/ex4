098c5cef3f733df41b7a1bbfb5e17947be3b9f45
shlomo4945 <shlomo4945@gmail.com>
2020-01-11 22:08:52 +0200

    ,

---
+++ cmake-build-debug/CMakeFiles/ex4.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/shlomo/CLionProjects/ex4/FileCacheManager.cpp" "/home/shlomo/CLionProjects/ex4/cmake-build-debug/CMakeFiles/ex4.dir/FileCacheManager.cpp.o"
+  "/home/shlomo/CLionProjects/ex4/MySerialServer.cpp" "/home/shlomo/CLionProjects/ex4/cmake-build-debug/CMakeFiles/ex4.dir/MySerialServer.cpp.o"
+  "/home/shlomo/CLionProjects/ex4/MyTestClientHandler.cpp" "/home/shlomo/CLionProjects/ex4/cmake-build-debug/CMakeFiles/ex4.dir/MyTestClientHandler.cpp.o"
+  "/home/shlomo/CLionProjects/ex4/main.cpp" "/home/shlomo/CLionProjects/ex4/cmake-build-debug/CMakeFiles/ex4.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
