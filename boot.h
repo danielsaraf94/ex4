97016588c785f94bb12bf0c345bcda0aea8e8ded
danielsaraf94 <osychod@gmail.com>
2020-01-11 21:23:01 +0200

    daniel

--- boot.h
+++ boot.h
@@ -1,23 +1,26 @@
 //
 // Created by daniels on 11/01/2020.
 //
+
 #ifndef EX4_5__BOOT_H_
 #define EX4_5__BOOT_H_
+#include "ClientHandler.h"
 #include "MySerialServer.h"
 #include "MyTestClientHandler.h"
-#include "StringReverser.h"
 #include "FileCacheManager.h"
+#include "StringReverser.h"
 using namespace std;
-namespace boot {
-  int main(int argc, char *argv[]) {
+namespace boot{
+class Main{
+ public:
+  int main(int argc,char* argv[]){
     MySerialServer server;
     StringReverser sr;
     FileCacheManager fcm;
-    MyTestClientHandler ch(&fcm, &sr);
-    server.open(atoi(argv[argc]), &ch);
-    while (true) {}
+    MyTestClientHandler ch(&fcm,&sr);
+    server.open(atoi(argv[argc]),&ch);
     return 0;
   }
+};
 }
-
 #endif //EX4_5__BOOT_H_
