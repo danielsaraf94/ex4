55ff282759006697bbe230aacfb10e7457f8e0d4
danielsaraf94 <osychod@gmail.com>
2020-01-12 18:31:52 +0200

    daniel

--- ASearcher.h
+++ ASearcher.h
@@ -5,8 +5,26 @@
 #ifndef EX4_5__ASEARCHER_H_
 #define EX4_5__ASEARCHER_H_
 #include "Searcher.h"
+#include "State.h"
+#include "StatePriorityQueue.h"
+#include <bits/stdc++.h>
+
+using namespace std;
 template<typename T>
 class ASearcher : public Searcher<T>{
  private:
+  StatePriorityQueue open_list;
+  int evaluatedNodes;
+ protected:
+  State<T> popOpenList(){
+    evaluatedNodes++;
+    return open_list.poll();
+  }
+ public:
+  ASearcher(){
+    open_list = new StatePriorityQueue();
+    evaluatedNodes=0;
+  }
+
 };
 #endif //EX4_5__ASEARCHER_H_
---
+++ StatePriorityQueue.h
@@ -0,0 +1,20 @@
+//
+// Created by daniels on 12/01/2020.
+//
+
+#ifndef EX4_5__STATEPRIORITYQUEUE_H_
+#define EX4_5__STATEPRIORITYQUEUE_H_
+#include "State.h"
+using namespace std;
+template<typename T>
+class StatePriorityQueue {
+ public:
+  int Count;
+  State<T> poll() {
+    return nullptr;
+  }
+  State<T> push() {
+    return nullptr;
+  }
+};
+#endif //EX4_5__STATEPRIORITYQUEUE_H_
