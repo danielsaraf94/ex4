6d2d1e05ba389035abbb34cb86db4421679e3661
shlomo4945 <shlomo4945@gmail.com>
2020-01-12 17:59:31 +0200

    ,

--- Searchable.h
+++ Searchable.h
@@ -5,10 +5,14 @@
 #ifndef EX4__SEARCHABLE_H_
 #define EX4__SEARCHABLE_H_
 #include "State.h"
+#include "list"
+using namespace std;
 template<typename T>
 class Searchable {
  public:
-  State<T>
+  virtual State<T> getInitialState()=0;
+  virtual State<T> getGoalState()=0;
+  virtual list<State<T>>getAllPossibleStates(State<T>S)=0;
 };
 
 #endif //EX4__SEARCHABLE_H_
