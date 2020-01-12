2e993c2dca62e3bd6d3311a9cd646fe744593145
shlomo4945 <shlomo4945@gmail.com>
2020-01-12 11:36:19 +0200

    ,

---
+++ Searchable.h
@@ -0,0 +1,12 @@
+//
+// Created by shlomo on 12/01/2020.
+//
+
+#ifndef EX4__SEARCHABLE_H_
+#define EX4__SEARCHABLE_H_
+
+class Searchable {
+
+};
+
+#endif //EX4__SEARCHABLE_H_
---
+++ Searcher.h
@@ -0,0 +1,12 @@
+//
+// Created by shlomo on 12/01/2020.
+//
+
+#ifndef EX4__SEARCHER_H_
+#define EX4__SEARCHER_H_
+template<typename T>
+class Searcher {
+ public:
+};
+
+#endif //EX4__SEARCHER_H_
