a012e6dfa35cf65a56383d8f2e11fe8452fa70d3
shlomo4945 <shlomo4945@gmail.com>
2020-01-13 08:26:29 +0200

    ,

--- BestFirstSearch.cpp
+++ BestFirstSearch.cpp
@@ -1,8 +1,23 @@
 //
 // Created by shlomo on 12/01/2020.
 //
-
 #include "BestFirstSearch.h"
-Solution<T> BestFirstSearch:: search(Searchable<T>)
-
+#include "list"
+Solution<T> BestFirstSearch:: search(Searchable<T> s){
+  ASearcher::PushOpenList(s.getInitialState());
+  while(!ASearcher::isemty()){
+    State<T> n=ASearcher::popOpenList();
+    this->closed.insert(n);
+    if(s.isGoalState(n)){
+      return backTrace();
+    }
+    list<State<T>> successors = s.getAllPossibleStates(n);
+    for(State s: successors){
+      if(!closed.count(s)&&ASearcher::openContaines(s)){
+        ASearcher::PushOpenList(s);
+      }else if(ASearcher::openContaines(S)){
+        if(ASearcher::)
+      }
+    }
+  }
 }
--- BestFirstSearch.h
+++ BestFirstSearch.h
@@ -5,14 +5,16 @@
 #ifndef EX4__BFS_H_
 #define EX4__BFS_H_
 #include "Solution.h"
+#include "State.h"
 #include "set"
 using namespace std;
 template<typename T>
 class BestFirstSearch: public ASearcher<T> {
-  set closed;
+  set<State<T>> closed;
+  Solution<T> backTrace();
  public:
   BestFirstSearch<T>():ASearcher<T>(){};
-  Solution<T> search(Searchable<T>)
+  Solution<T> search(Searchable<T>);
 };
 
 #endif //EX4__BFS_H_
